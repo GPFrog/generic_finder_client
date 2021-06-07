9a93218887be4ebbcf7cd50e69f652817716ca4b
ssh-24 <20160641@kumoh.ac.kr>
2021-06-07 20:30:37 +0900

    0607
    0607

--- app/src/main/java/com/example/genericfinder/SignupFragment.java
+++ app/src/main/java/com/example/genericfinder/SignupFragment.java
@@ -37,6 +37,9 @@ public class SignupFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_signup, container, false);
 
+        RequestTask requestTask = new RequestTask();
+        String url = "http://152.70.89.118:4321/";
+
         emailEdit = view.findViewById(R.id.emailEdit);
         codeEdit = view.findViewById(R.id.codeEdit);
         requestBtn = view.findViewById(R.id.requestBtn);
@@ -52,11 +55,26 @@ public class SignupFragment extends Fragment {
                     Toast.makeText(getContext(),"Email을 입력해주세요.",Toast.LENGTH_SHORT).show();
                 }
                 else {
-                    // ~~~~ 인증 코드 요청받는 코드
+                    //인증 코드 요청받는 거
+                    String rtResult = null;
 
+                    try {
+                        rtResult = requestTask.execute(url + "email?email=\"" + emailEdit.getText().toString()+"\"").get();
+                        rtResult = rtResult.replaceAll("\"", "");
+                        String arr[] = rtResult.split("/");
 
-                    //성공했으면
-                    Toast.makeText(view.getContext(),"인증 코드를 발송했습니다.",Toast.LENGTH_SHORT).show();
+                        //성공했을 때
+                        if (arr[0].compareTo("ok")==0){
+                            //성공했으면
+                            Toast.makeText(view.getContext(),"인증 코드를 발송했습니다.",Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            //실패했으면
+                            Toast.makeText(view.getContext(),"다시 요청하세요.",Toast.LENGTH_SHORT).show();
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         });
@@ -65,40 +83,41 @@ public class SignupFragment extends Fragment {
         confirmBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                RequestTask requestTask = new RequestTask();
-//                String rtResult = null;
-//
-//                try {
-//                    rtResult = requestTask.execute("", "userEmail=" + emailEdit.toString()).get();
-//                    JSONObject jsonObject = new JSONObject(rtResult);
-//
-//                    if(jsonObject.toString().contains("true")) {
-//                        Toast.makeText(view.getContext(), "회원가입이 완료되었습니다.", Toast.LENGTH_SHORT).show();
-//                        ((MainActivity)getActivity()).replaceFragment(enterFragment);
-//                    }
-//                    else Toast.makeText(view.getContext(), "회원가입에 실패했습니다.", Toast.LENGTH_SHORT).show();
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-//                }
-
                 //인증 번호 미입력 시
                 if (codeEdit.getText().toString().isEmpty()){
                     Toast.makeText(getContext(),"인증에 실패하였습니다.",Toast.LENGTH_SHORT).show();
                 }
                 else {
                     //인증 번호가 일치할 때
+                    // ~~~~ 인증 코드 요청받는 코드
+                    String rtResult = null;
 
-                    //이메일 넘기기 위해 저장
-                    SharedPreferences sharedEmail = getContext().getSharedPreferences("email",Context.MODE_PRIVATE);
-                    SharedPreferences.Editor editor = sharedEmail.edit();
-                    editor.putString("signupEmail",emailEdit.getText().toString()); //key, value 형식으로 저장
-                    editor.commit();
-                    Toast.makeText(view.getContext(),"저장 성공!",Toast.LENGTH_SHORT).show();
+                    try {
+                        rtResult = requestTask.execute(url + "certification?code=" + codeEdit.getText().toString()).get();
+                        rtResult = rtResult.replaceAll("\"", "");
+                        String arr[] = rtResult.split("/");
 
-                    Toast.makeText(view.getContext(),"인증에 성공하였습니다.",Toast.LENGTH_SHORT).show();
+                        //성공했을 때
+                        if (arr[0].compareTo("success")==0){
+                            //성공했으면
+                            //이메일 넘기기 위해 저장
+                            SharedPreferences sharedEmail = getContext().getSharedPreferences("email",Context.MODE_PRIVATE);
+                            SharedPreferences.Editor editor = sharedEmail.edit();
+                            editor.putString("signupEmail",emailEdit.getText().toString()); //key, value 형식으로 저장
+                            editor.commit();
 
-                    //다음 화면으로 전환
-                    ((MainActivity)getActivity()).replaceFragment(signupFragment2);
+                            Toast.makeText(view.getContext(),"인증에 성공하였습니다.",Toast.LENGTH_SHORT).show();
+
+                            //다음 화면으로 전환
+                            ((MainActivity)getActivity()).replaceFragment(signupFragment2);
+                        }
+                        else{
+                            //실패했으면
+                            Toast.makeText(view.getContext(),"인증 실패",Toast.LENGTH_SHORT).show();
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         });
